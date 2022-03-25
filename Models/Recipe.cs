e4a13cff1041446ed83bb1004eb5374e657fd6e4
Jo Jo Lam <jojilam@gmail.com>
2022-03-24 22:38:46 -0700

    salvaged pages

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,25 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace RecipeApp.Models
 {
-    public class Recipe
-    {
-        public int id { get; set; }
-        public string? title { get; set; }
-        public string? image { get; set; }
-        public string? imageType { get; set; }
-        public float? servings { get; set; }
-        public int? readyInMinutes { get; set; }
-        public string? license { get; set; }
-        public string? sourceName { get; set; }
-        public string? sourceUrl { get; set; }
-        public string? spoonacularSourceUrl { get; set; }
-        public float? aggregateLikes { get; set; }
-        public float? healthScore { get; set; }
-        public float? spoonacularScore { get; set; }
-        public float? pricePerServing { get; set; }
-        public string? instructions { get; set; }
-        public bool? cheap { get; set; }
-        public string? creditsText { get; set; }
-        public string? summary { get; set; }
-        public List<Ingredient>? extendedIngredients { get; set; }
-    }
+  public class Recipe
+  {
+    [Key]
+    public int RecipeId { get; set; }
+    [ForeignKey("ApplicationUser")]
+    public string Id { get; set; }
+    [Required]
+    [Display(Name = "Recipe Title")]
+    public string? Title { get; set; }
+    [Required]
+    public string? Description { get; set; }
+    [Display(Name = "Image URL")]
+    public string? Image { get; set; }
+    [Required]
+    public string? Steps { get; set; }
+    [Required]
+    public string Ingredients { get; set; }
+  }
 }
---
+++ Models/WeekPlan.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeApp.Models
+{
+  public class WeekPlan
+  {
+    public int WeekPlanId { get; set; }
+    [ForeignKey("ApplicationUser")]
+    public string Id { get; set; }
+    public List<Recipe>? Monday { get; set; }
+    public List<Recipe>? Tuesday { get; set; }
+    public List<Recipe>? Wednesday { get; set; }
+    public List<Recipe>? Thursday { get; set; }
+    public List<Recipe>? Friday { get; set; }
+    public List<Recipe>? Saturday { get; set; }
+    public List<Recipe>? Sunday { get; set; }
+
+  }
+}
